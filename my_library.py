0e7a8de6cf819f6b3be979c58862abbdd68e211e
NSKZ <nicks252@my.yorku.ca>
2020-03-26 05:29:14 -0700

    done

--- my_library.py
+++ my_library.py
@@ -1,8 +1,12 @@
 import math
 
 class Vector:
-    def __init__(self, arguments):
-        self.arguments = arguments
+    def __init__(self, arguments = []):
+        try:
+            assert arguments.__class__.__name__ == "list"
+            self.arguments = arguments
+        except:
+            Exception("Arguments of a vector must be of type 'list'.")
        
     def __add__(vector1, vector2):
         if Vector.check_if_equal(vector1, vector2):
@@ -67,6 +71,9 @@ class Vector:
                     return False
             return True
         
+    def __call__(self):
+        return self.arguments
+        
     def append_elements(self, *elements):
         for element in elements:
             self.arguments.append(element)
@@ -85,6 +92,14 @@ class Vector:
             for i in range(len(vector1)):
                 sum += vector1[i] * vector2[i]
             return sum
+        
+    @classmethod
+    def zeros(cls, num):
+        return cls([0]*num)
+    
+    @classmethod
+    def constant(cls, var, num):
+        return cls([var]*num)
     
     @classmethod
     def angle(cls, vector1, vector2):
@@ -122,15 +137,56 @@ class Vector:
     
     @classmethod
     def cross_product(cls, vector1, vector2):
-        pass
-    
+        if Vector.check_if_equal(vector1, vector2) and len(vector1) == 3:
+            result = Vector([0] * 3)
+            for i in range(3):
+                result[i] = cross_multiply(vector1, vector2, range_except(2, i))
+                if i%2 != 0:
+                    result[i] = -result[i]
+            return result
+        else:
+            raise Exception("Cross-multiplication can only be done with two vectors of length 3.")
    
+class Matrix:
+    def __init__(self, vectors = []):
+        for vector in vectors:
+            len_vector = len(vector[0])
+            if vector.__class__.__name__ not in ["list", "Vector"]:
+                Exception("Matrices must be made up of vectors or list datatypes.")
+            if len(vector) != len_vector:
+                Exception("All vectors constituting a matrix must have the same length.")
+        self.vectors = vectors
+        
+
+def boolean_flip(boolean):
+    if boolean is True:
+        boolean = False
+    else: boolean = True
+    return boolean
+
+def cross_multiply(a, b, index):
+    return a[index[0]]*b[index[1]] - b[index[0]]*a[index[1]]
+
+def range_except(_range, num):
+    result = []
+    for i in range(_range + 1):
+        if i != num:
+            result.append(i)
+    result = tuple(result)
+    return result
+
 def is_between(number, boundary1, boundary2):
     boundary1, boundary2 = min(boundary1, boundary2), max(boundary1, boundary2)
     if number >= boundary1 and number <= boundary2:
         return True
     return False
 
-vector = Vector([4, 5])
-vector.move([1, 1], 5)
-print(vector)
+def time_it(function, num_times):
+    #Run as: time_it(function = lambda: boolean_flip(bool_var), num_times = 10000000)
+    import time
+    start = time.time()
+    for i in range(num_times):
+        function()
+    end = time.time()
+    return end - start
+
